e0030147e701397228679bf74e66d46dbf63927f
ayush <ayushwilltry@gmail.com>
2023-09-28 21:49:53 +0545

    cjs

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,17 +1,20 @@
-import express from "express";
-import jsonwebtoken from "jsonwebtoken";
-
+const express = require('express');
+const jsonwebtoken = require('jsonwebtoken');
 
 const router = express.Router();
 
-router.post("/sign-in", (req, res) => {
+router.post('/sign-in', (req, res) => {
   // Authenticate
-  const username = req.body.username
-  console.log(username)
-  const user = {name : username}
-  const token = jsonwebtoken.sign(user , process.env.access_token_secret);
+  const username = req.body.username;
+  console.log(username);
+  const user = { name: username };
+  const token = jsonwebtoken.sign(user, process.env.access_token_secret);
 
   res.send(token);
 });
 
-export default router; // Export the router as a default export
+router.get("/sign-in" , (req , res) => {
+  res.send("authCONtroller")
+})
+
+module.exports = router; // Export the router as a default export
--- index.js
+++ index.js
@@ -1,63 +1,50 @@
-import express from 'express.js'
-// // import authRouter from './controllers/auth/authController.js';
-// // import cors from 'cors'
-// // import dotenv from 'dotenv'
-// // import { authenticateToken } from './middleware/authMiddleWare.js';
+const express = require('express');
+const { urlencoded } = require('express');
+const authRouter = require('./controllers/auth/authController.js');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { authenticateToken } = require('./middleware/authMiddleWare.js');
 
-// // dotenv.config();
+dotenv.config();
 
+const app = express();
+const router = express.Router();
 
-// const app = express();
-// const router = express.Router();
+app.use(urlencoded({ extended: false }));
 
-// // app.use(urlencoded({extended:false}))
+app.use(
+  cors({
+    allowedHeaders: ['sessionId', 'Content-Type', 'Authorization', 'authorization'],
+    exposedHeaders: ['sessionId'],
+    origin: ['https://eccentrictoad.com', 'https://www.eccentrictoad.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+    preflightContinue: false,
+  })
+);
 
-// // app.use(
+app.use(router);
+app.use(express.json());
+app.use('/api/auth', authRouter);
 
-// //   cors({
-// //     'allowedHeaders': ['sessionId', 'Content-Type', 'Authorization', 'authorization'],
-// //     'exposedHeaders': ['sessionId'],
-// //     'origin': ['https://eccentrictoad.com', 'https://www.eccentrictoad.com'],
-// //     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-// //     'credentials': false,
-// //     'preflightContinue': false
-// //   })
+app.get('/', (req, res) => {
+  res.json({ name: 'ayush' });
+});
 
-// // )
+const users = [
+  {
+    username: 'ayush',
+  },
+  {
+    username: 'ankit',
+  },
+];
 
-// app.use(router);
-// // app.use(express.json())
-// // app.use('/api/auth' , authRouter);
-
-// router.get("/" ,(req , res) => {
-//   res.json({name:"ayush"})
-// })
-
-// // const users =[
-// //   {
-// //     username:"ayush"
-// //   },
-// //   {
-// //     username:"ankit"
-// //   }
-// // ]
-
-// // app.post("/api/post" , authenticateToken , (req , res) => {
-// //   const response = users.filter(post => {
-// //     return post.username == req.user.name;
-    
-// //   })
-// //   res.json(response);
-// // })
-
-// app.listen(3001)
-
-// const express = require("express")
-
-const app = express()
-
-app.get("/" , (req , res) => {
-  res.send("hello" + process.env.access_token_secret)
-})
+app.post('/api/post', authenticateToken, (req, res) => {
+  const response = users.filter((post) => {
+    return post.username == req.user.name;
+  });
+  res.json(response);
+});
 
 app.listen(3001);
