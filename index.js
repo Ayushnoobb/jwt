7b8ff8f4d42ae0b1e587f7b6fa063d5adf7d38fe
ayush <ayushwilltry@gmail.com>
2023-09-28 20:35:22 +0545

    cors-issue

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const express = require("express")
 const app = express()
 
 app.get("/" , (req , res) => {
-  res.send("hello")
+  res.send("hello" + process.env.access_token_secret)
 })
 
 app.listen(3001);
